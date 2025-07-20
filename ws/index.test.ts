5ee1f4b6dbcbdcff1d2f33c29bc3a49e703344eb
sandeep0009 <500sandeepks@gmail.com>
2025-07-21 00:48:29 +0530

    feat:added test

---
+++ ws/index.test.ts
@@ -0,0 +1,57 @@
+import {describe, expect, test} from "bun:test";
+import WebSocket from "ws";
+
+
+
+const BACKEND_URL='ws://localhost:8080';
+
+describe('Chat application', () => {
+
+    test('sending message to chat room1 to room2',async()=>{
+        const ws1= new WebSocket(BACKEND_URL);
+        const ws2= new WebSocket(BACKEND_URL);
+
+
+        const openWS1= new Promise<void>((resolve)=>{
+            ws1.onopen=()=>resolve();
+        });
+
+        const openWS2=new Promise<void>((resolve)=>{
+            ws2.onopen=()=>resolve();
+        });
+        await Promise.all([openWS1,openWS2]);
+
+        ws1.send(JSON.stringify({
+            type:'join-room',
+            room:'Room 1'
+        }));
+
+        ws2.send(JSON.stringify({
+            type:'join-room',
+            room:'Room 1'
+        }));
+
+
+
+        await new Promise<void>((resolve)=>{
+
+             ws2.onmessage=({data})=>{
+                console.log('data',data);
+            const parsedData=JSON.parse(data);
+            expect(parsedData.type="chat");
+            expect(parsedData.message="hi there");
+            resolve();
+        }
+        ws1.send(JSON.stringify({
+            type:'chat',
+            room:'Room 1',
+            message:'hi there'
+        }));
+
+        })
+
+        
+
+       
+    })
+ })
