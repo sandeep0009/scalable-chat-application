5deb0dcc7e03b8f71dfdb872d581e2b092c4f129
sandeep0009 <500sandeepks@gmail.com>
2025-07-21 00:21:57 +0530

    feat:added simple chat logic

--- ws/index.ts
+++ ws/index.ts
@@ -1,11 +1,30 @@
 import { WebSocketServer } from 'ws';
+import type { Room } from './types';
 
 const wss = new WebSocketServer({ port: 8080 });
 
+
+const rooms:Record<string, Room>={};
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
-  ws.on('message', function message(data) {
+  ws.on('message', function message(data:string) {
+    const parsedData=JSON.parse(data);
+    if(parsedData.type=="join-room"){
+        const room=parsedData.room;
+        if(!rooms[room]){
+            rooms[room]={
+                socket:[]
+            }
+
+        }
+        rooms[room].socket.push(ws);
+    }
+    if(parsedData.type=="chat"){
+        const room=parsedData.room;
+        rooms[room].socket.map(sc=>sc.send(data));
+    }
+
     console.log('received: %s', data);
   });
 
